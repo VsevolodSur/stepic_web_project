3a5e1fef86a0140c17086d117d7a1d4372c2dc1c
Vsevolod Suryaninov <giwecm@gmail.com>
2018-01-26 08:49:56 +0300

    First working config

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 echo "init script"
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf 
+if [ -f /etc/nginx/conf.d/default.conf ]; then 
+    sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
+fi
+sudo /etc/init.d/nginx restart
+
