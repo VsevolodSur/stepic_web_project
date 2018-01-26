0ab3335a862f4c8b7cf417e0fa311f4f2581b3e5
Vsevolod Suryaninov <giwecm@gmail.com>
2018-01-26 10:50:14 +0300

    Add process catalog /etc/nginx/sites-enabled

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stepic_web_project
 Repository for Stepic course  "Web technology"
+
+2018/01/26
+Simple static website (nginx) 
--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 echo "init script"
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf 
+if [ ! -L /etc/nginx/conf.d/test.conf ]; then
+    sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf 
+fi
 if [ -f /etc/nginx/conf.d/default.conf ]; then 
     sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
+fi
+if [ -f /etc/nginx/sites-enabled/default ]; then 
+    sudo rm /etc/nginx/sites-enabled/default
 fi
 sudo /etc/init.d/nginx restart
 
