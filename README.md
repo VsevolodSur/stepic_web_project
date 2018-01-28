44e3c826acf2884d002c4fefe6011f69f1a8b6db
Vsevolod Suryaninov <giwecm@gmail.com>
2018-01-28 16:48:59 +0300

    Add using app guincorn

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Repository for Stepic course  "Web technology"
 
 2018/01/26
-Simple static website (nginx) 
+Simple static website (nginx)
+2018/01/28
+Add application server gunicorn
--- init.sh
+++ init.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-echo "init script"
-if [ ! -L /etc/nginx/conf.d/test.conf ]; then
-    sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf 
+echo "Init script"
+
+if [ ! -L /etc/gunicorn/hello.py ]; then
+    sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 fi
-if [ -f /etc/nginx/conf.d/default.conf ]; then 
+if [ ! -L /etc/nginx/conf.d/test.conf ]; then
+    sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf
+fi
+if [ -f /etc/nginx/conf.d/default.conf ]; then
     sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
 fi
-if [ -f /etc/nginx/sites-enabled/default ]; then 
+if [ -f /etc/nginx/sites-enabled/default ]; then
     sudo rm /etc/nginx/sites-enabled/default
 fi
-sudo /etc/init.d/nginx restart
 
+ls -la /etc/gunicorn.d/hello.py /etc/nginx/conf.d/test.conf
+
+sudo /etc/init.d/nginx restart
