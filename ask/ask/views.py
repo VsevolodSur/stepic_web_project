6c2f7c6b3d4695740f70ab4adb48c6d1b91dc1ab
Vsevolod Suryaninov <giwecm@gmail.com>
2018-02-04 11:30:06 +0300

    Add gunictl + modify 404 response

--- ask/ask/views.py
+++ ask/ask/views.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from django.http import HttpResponse, Http404 
 
 def myroot(request, *args, **kwargs):
@@ -7,4 +9,9 @@ def ask(request, *args, **kwargs):
     return HttpResponse("It's ask!")
 
 def err404(request, *args, **kwargs):
-    raise Http404()
+    # raise Http404()
+    return HttpResponse(
+        content = "<html><head><meta charset='utf-8'>Ничего такого нет...</head></html>",
+        content_type = "text/html",
+        status = 404,)
+
---
+++ gunictl.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function run_gunicorn () {
+    echo "Start gunicorn processes..."
+
+    gunicorn -c /etc/gunicorn.d/hello.py hello:app & 
+    cd ask/ 
+    gunicorn -c /etc/gunicorn.d/ask.py ask.wsgi:application & 
+}
+
+function kill_gunicorn {
+    echo "Kill gunicorn processes..."
+
+    for pid in `ps -ef | grep gunicorn | grep -v grep | awk '{ print $2 }'` 
+    do
+        echo " kill pid: " $pid
+        kill -9 $pid
+    done
+}
+
+case $1 in
+    "start" )
+    run_gunicorn
+        ;;
+    "stop" )
+    kill_gunicorn
+        ;;
+    "restart" )
+    kill_gunicorn
+    run_gunicorn
+    ;;
+    * )
+    echo "Format: $0 start|stop|restart"
+    exit 1
+    ;;
+esac
