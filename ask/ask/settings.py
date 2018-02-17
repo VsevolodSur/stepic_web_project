4d7f95dbbe91d4e444fc9896f90f0a5eb88df40e
Vsevolod Suryaninov <giwecm@gmail.com>
2018-02-17 08:20:13 +0300

    Add ForeignKey on fields author ref django.contrib.auth.models.User

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -109,20 +109,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
