d0de879518ef499e0664d7e1c3eacbd834625576
Vsevolod Suryaninov <giwecm@gmail.com>
2018-06-22 14:14:14 +0300

    Add python3 venv

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ homeipaddr=192.168.1.201
 IPADDR=`ifconfig | grep addr: | grep -v "127.0.0.1" | awk '{ print substr($2, 6) }'`
 HOSTNAME=`hostname`
 WORKDIR=/home/box/web
+ENVDIR=${WORKDIR}/env
 
 if [ -L /etc/gunicorn.d/ask.py ]; then
 	sudo rm /etc/gunicorn.d/ask.py
@@ -16,6 +17,16 @@ fi
 if [ -L /etc/nginx/conf.d/test.conf ]; then
 	sudo rm /etc/nginx/conf.d/test.conf
 fi
+if [ -d ${ENVDIR}/ ]; then
+	rm -rf ${ENVDIR}
+fi
+
+# virtualenv --python=/usr/bin/python3 ${ENVDIR}
+python3 -m venv ${ENVDIR}
+. ${ENVDIR}/bin/activate
+pip install Django==2.0.6
+pip install pymysql
+# pip install gunicorn
 
 if [ $HOSTNAME = $host ] && [ $IPADDR != $homeipaddr ]; then # on 636
 	sed "s/${homeipaddr}/localhost/" ${WORKDIR}/etc/ask_cfg.py > ${WORKDIR}/etc/ask.py
@@ -31,18 +42,14 @@ elif [ $HOSTNAME != $host ]; then #on terminal
 	sed "s/${homeipaddr}/0.0.0.0/" ${WORKDIR}/etc/hello_cfg.py > ${WORKDIR}/etc/hello.py
 	sed "s/${homeipaddr}/localhost/" ${WORKDIR}/etc/nginx_cfg.py > ${WORKDIR}/etc/nginx.conf
 	sed -i -e "s/DEBUG = True/DEBUG = False/; s/ALLOWED_HOSTS = \[.*\]/ALLOWED_HOSTS = \[\x27*\x27\]/" ${WORKDIR}/ask/ask/settings.py
-	# virtualenv --python=/usr/bin/python3 /home/box/web/env
-  # . env/bin/activate
-	# pip install Django==2.0.6
-	# pip install pymysql
 else
 	echo "Unknown host&ip" ${HOSTNAME} ${IPADDR}
 	exit 1
 fi
 
-sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf
+sudo ln -s ${WORKDIR}/etc/ask.py /etc/gunicorn.d/ask.py
+sudo ln -s ${WORKDIR}/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s ${WORKDIR}/etc/nginx.conf /etc/nginx/conf.d/test.conf
 
 if [ -f /etc/nginx/conf.d/default.conf ]; then
     sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
@@ -64,3 +71,4 @@ mysql -u root -p < mysql_init.sql
 ask/manage.py makemigrations qa
 
 ask/manage.py migrate
+${WORKDIR}/gunictl.sh restart
